94899dfad2ca565eaafc22b162d7a79a7d352b83
Ronny Soltveit <ronny@Ronnys-Air.home>
2015-11-12 23:36:59 +0100

    First build

--- Swipe Gesture/ViewController.swift
+++ Swipe Gesture/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblDirection: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipe:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        view.addGestureRecognizer(swipeRight)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipe:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        view.addGestureRecognizer(swipeDown)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipe:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        view.addGestureRecognizer(swipeUp)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipe:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        view.addGestureRecognizer(swipeLeft)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func respondToSwipe(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                lblDirection.text = "Right"
+            case UISwipeGestureRecognizerDirection.Left:
+                lblDirection.text = "Left"
+            case UISwipeGestureRecognizerDirection.Up:
+                lblDirection.text = "Up"
+            case UISwipeGestureRecognizerDirection.Down:
+                lblDirection.text = "Down"
+            default:
+                break
+            }
+        }
     }
-
-
 }
 
